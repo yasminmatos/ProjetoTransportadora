7750dc431559e1b7f87377003b5e6aeef2e53a3b
Yasmin Matos <93132290+yasminmatos@users.noreply.github.com>
2021-12-09 10:44:53 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,31 @@
+package classe;
+
+import classe.Veiculos;
+
+public class Carro extends Veiculos {
+		private int quantidadeLugares;
+		//construtor
+
+		public Carro(String placa, String tipoCombustivel, String fabricante, String numeroChassis, String cor,
+				String tipoVeiculo, String modelo, String anoFabricacao, String anoModelo, int quantidadeLugares) {
+			super(placa, tipoCombustivel, fabricante, numeroChassis, cor, tipoVeiculo, modelo, anoFabricacao, anoModelo);
+			this.quantidadeLugares = quantidadeLugares;
+		}
+		public String mostrarDados() {
+			return "Tipo Veiculo: "+ this.tipoVeiculo +
+					"\nFabricante: "+ this.fabricante +
+					"\nPlaca: "+this.placa +
+					"\nCombustivel: "+ this.tipoCombustivel +
+					"\nAno Fabricação: "+ this.anoFabricacao +
+					"\nModelo: " + this.modelo +
+					"\nAno do Modelo: "+ this.anoModelo +
+					"\n Cor: "+ this.cor +
+					"\nQuantidade de Lugares: " + this.quantidadeLugares+
+					"\nNúmero de chassi: "+this.numeroChassis;
+			}
+
+		}
+
+	
+
+
---
+++ Onibus.java
@@ -0,0 +1,23 @@
+package classe;
+
+public class Onibus extends Veiculos {
+private int QuantidadePoltronas;
+
+public Onibus(String placa, String tipoCombustivel, String fabricante, String numeroChassis, String cor,
+		String tipoVeiculo, String modelo, String anoFabricacao, String anoModelo, int quantidadePoltronas) {
+	super(placa, tipoCombustivel, fabricante, numeroChassis, cor, tipoVeiculo, modelo, anoFabricacao, anoModelo);
+	QuantidadePoltronas = quantidadePoltronas;
+}
+public String mostrarDados() {
+	return "Tipo Veiculo: "+ this.tipoVeiculo +
+			"\nQuantidade de Poltronas: "+ this.QuantidadePoltronas +
+			"\nFabricante: "+ this.fabricante +
+			"\nPlaca: "+this.placa +
+			"\nCombustivel: "+ this.tipoCombustivel +
+			"\nAno Fabricação: "+ this.anoFabricacao +
+			"\nModelo: " + this.modelo +
+			"\nAno do Modelo: "+ this.anoModelo +
+			"\n Cor: "+ this.cor +
+			"\nNúmero de chassi: "+this.numeroChassis;
+	}
+}
